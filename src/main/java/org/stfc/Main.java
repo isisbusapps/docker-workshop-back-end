b7811797eacbac30debeba148429b723af54e4a3
Cosimo Campo <cosimo.campo@stfc.ac.uk>
2021-03-12 17:41:17 +0000

    Make app container-friendly

--- src/main/java/org/stfc/Main.java
+++ src/main/java/org/stfc/Main.java
@@ -4,12 +4,11 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
 import java.net.URI;
 
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:5000/";
+    public static final String BASE_URI = "http://0.0.0.0:5000/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -25,12 +24,11 @@ public class Main {
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+        System.out.println(String.format(
+                "Jersey app started with WADL available at %sapplication.wadl.",
+                BASE_URI));
     }
 }
 
