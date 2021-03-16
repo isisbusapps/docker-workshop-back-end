a8ce4b6344d1798d9eae28f065185953158c32f5
Cosimo Campo <cosimo.campo@stfc.ac.uk>
2021-03-16 09:26:26 +0000

    Use BisAppSettings for DB credentials

--- src/main/java/org/stfc/datasource/DatabaseDataSource.java
+++ src/main/java/org/stfc/datasource/DatabaseDataSource.java
@@ -3,6 +3,7 @@ package org.stfc.datasource;
 import oracle.jdbc.driver.OracleConnection;
 import oracle.jdbc.pool.OracleDataSource;
 import org.stfc.model.Proposal;
+import uk.stfc.bisapps.config.BISAppProperties;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,11 +15,13 @@ public class DatabaseDataSource implements DataSource {
     private OracleConnection connection;
 
     public DatabaseDataSource() {
+        BISAppProperties properties = new BISAppProperties("C:\\FBS\\BISAppSettings\\BISAppSettings.config");
+
         try {
             OracleDataSource dataSource = new OracleDataSource();
-            dataSource.setURL("jdbc:oracle:thin:@//localhost:1521/xepdb");
-            dataSource.setUser("iops_v4");
-            dataSource.setPassword("pa55w0rdTolocalDB");
+            dataSource.setURL(properties.getProperty("DOCKERWORKSHOP.DBURL"));
+            dataSource.setUser(properties.getProperty("DOCKERWORKSHOP.DBUser"));
+            dataSource.setPassword(properties.getProperty("DOCKERWORKSHOP.DBPassword"));
 
             connection = (OracleConnection) dataSource.getConnection();
         } catch (SQLException ex) {
@@ -33,10 +36,9 @@ public class DatabaseDataSource implements DataSource {
         try {
             Statement stm = connection.createStatement();
             ResultSet results = stm.executeQuery("" +
-                    "SELECT * " +
-                    "FROM proposal " +
-                    "ORDER BY submitted_date DESC " +
-                    "FETCH FIRST 10 ROWS ONLY"
+                    "SELECT * FROM " +
+                    "(SELECT * FROM proposal ORDER BY submitted_date DESC) " +
+                    "WHERE ROWNUM <= 10"
             );
 
             while (results.next()) {
